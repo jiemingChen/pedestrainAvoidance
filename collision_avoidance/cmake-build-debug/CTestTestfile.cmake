bf428445896a2bdb66bca24aa0cd3fb63472efa9
jasonchenj <15001781672@163.com>
2020-05-31 22:16:07 +0200

    mpc

--- collision_avoidance/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jieming/car_ws/src/collision_avoidance
-# Build directory: /home/jieming/car_ws/src/collision_avoidance/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
